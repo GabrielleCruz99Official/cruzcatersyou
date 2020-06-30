e0e3413855007ef253bddcdb3ad9f46ce42d68c0
Gabrielle Cruz <47248597+JhandarTheUnhallowed@users.noreply.github.com>
2020-06-30 14:36:38 +0200

    update 4 - 30/06

--- js/setmenu.js
+++ js/setmenu.js
@@ -1,12 +1,34 @@
 "use strict"
 
-let testMenu = ["Kare-Kare", "Beef Caldereta", "Five-Spice Chicken"];
-function testItem(){
+let sampleMenu = {
+    'Kare-Kare': 10,
+    'Beef Caldereta': 12,
+    'Cheesecake': 6
+};
+function loadSample(){
     let addTable = '';
-    addTable += "<thead><tr><th>Item</th><th>Add?</th></tr></thead><tbody>"
-    for(let item of testMenu){
-        addTable += "<tr><td>" + item + "</td><td><button onclick='addItem()'>Add</button></td></tr>";
+    addTable += "<thead><tr><th>Item</th><th>Price</th><th>Add?</th></tr></thead><tbody>";
+    for(let item of Object.keys(sampleMenu)){
+        addTable += "<tr><td>" + item + "</td><td>" + sampleMenu[item]
+            + "€</td><td><button onclick='addItem()'>Add</button></td></tr>";
     }
     addTable += "</tbody>";
     gid('menuchoices').innerHTML = addTable;
+}
+
+function loadSetMenu(){
+    loadSample();
+}
+
+function addItem(item){
+    let addChoice = '';
+
+    addChoice += "<thead><tr><th>Item</th><th>Remove?</th></tr></thead><tbody>";
+    addChoice += "<tr id=" + item + "><td>" + item + "</td><td><button onclick='removeItem(item)'>Add</button></td></tr>";
+    addChoice += "</tbody>";
+    gid('menupicked').innerHTML = addChoice;
+}
+
+function removeItem(){
+
 }
