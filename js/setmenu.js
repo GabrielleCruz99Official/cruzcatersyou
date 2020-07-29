21680d80c2205c2422a5d759bcd52fee2adde222
Gabrielle Cruz <47248597+JhandarTheUnhallowed@users.noreply.github.com>
2020-07-29 16:41:56 +0200

    reussite de sauvegarde

--- js/setmenu.js
+++ js/setmenu.js
@@ -1,9 +1,6 @@
 "use strict"
+/* clear previous menu and load item cache */
 
-/* event listeners */
-const pageLoad = document.addEventListener('load', loadMenuList);
-
-/* Load Menu Cache and Clear Previously Set Table */
 let menu = [];
 let menuList = new XMLHttpRequest();
 menuList.open("get", "/weekmenu", true);
@@ -22,7 +19,16 @@ clear.onload = function(){
 };
 clear.send();
 
-/* Load Cache onto Webpage */
+/* variables */
+let pickedMenu = []; //table containing the chosen items
+let btnSubmit = $('.menuSubmit'); //for modal popup
+
+/* functions */
+
+function load(){
+    loadMenuList();
+}
+
 function loadMenuList() {
     let APP = '';
     let MAIN = '';
@@ -30,36 +36,80 @@ function loadMenuList() {
     for (let id in menu) {
         if(menu[id].itemCourse == 'Appetizer'){
             APP += "<input type='checkbox' class='menu' value='"
-                + menu[id].itemID + "'>"
+                + menu[id].itemName + "'>"
                 + menu[id].itemName + " : "
                 + menu[id].itemPrice + "€ <br>";
         }else if(menu[id].itemCourse == 'Main') {
             MAIN += "<input type='checkbox' class='menu' value='"
-                + menu[id].itemID + "'>"
+                + menu[id].itemName + "'>"
                 + menu[id].itemName + " : "
                 + menu[id].itemPrice + "€ <br>";
         }else if(menu[id].itemCourse == 'Dessert') {
             DESSERT += "<input type='checkbox' class='menu' value='"
-                + menu[id].itemID + "'>"
+                + menu[id].itemName + "'>"
                 + menu[id].itemName + " : "
                 + menu[id].itemPrice + "€ <br>";
         }else{
             console.log('Error!');
         }
+
     }
     $('#appetizer').innerHTML = APP;
     $('#main').innerHTML = MAIN;
     $('#dessert').innerHTML = DESSERT;
 }
 
-let pickedMenu = []; //Array containing the chosen items for this week's menu; to be submitted later
-
-/* save menu for the week */
-function saveMenu(){
-    alert("This week's menu has been set!");
-    window.location.replace("/");
+function confirmation() {
+    stockMenu();
+    $('.popup').style.display = "block";
+    return false;
 }
 
-function load(){
-    loadMenuList();
+function stockMenu(){
+    let submitList = $('.chosenMenu');
+    let displayList = '';
+    let chosenList = $all('input[type=checkbox]:checked');
+    for (let chosen of chosenList) {
+        for (let id in menu) {
+            if (chosen.value == menu[id].itemName) {
+                pickedMenu.push({
+                    chosenItemID: menu[id].itemID,
+                    chosenItem: menu[id].itemName,
+                    chosenItemPrice: menu[id].itemPrice
+                });
+                displayList += "<li>" + menu[id].itemName + "</li>";
+            }
+        }
+    }
+    submitList.innerHTML = displayList;
+    console.log(pickedMenu);
+}
+
+function saveMenu(){
+    let confirmMenu = confirm("Is the menu finalized?");
+    if(confirmMenu){
+        for(let item of pickedMenu){
+            let url = setURL(item.chosenItemID);
+            let save = new XMLHttpRequest();
+            save.open("get", url, false);
+            save.onreadystatechange = function(){
+                console.log(this.readyState + " " + this.status);
+                if(this.readyState == 4 & this.status == 200){
+                    if(JSON.parse(this.responseText)[0] == 200){
+                        console.log('Item added to this week\'s menu!');
+                    }
+                }
+            }
+            save.send();
+        }
+        alert('Menu is set for this week!');
+        window.location.replace("/");
+    }else{
+        $('.popup').style.display = "none";
+    }
+}
+
+function setURL(value){
+    let urlBase = '/setweek?chosenItemID=' + value;
+    return urlBase;
 }
